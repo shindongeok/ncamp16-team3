bec9d75a0054a6e0572836ae5a1e99dbb8704d33
hahyeong <hahyeong_0817@ewhain.net>
2025-02-10 17:41:44 +0900

    project setting

---
+++ iz/src/main/java/com/iz/IzApplication.java
@@ -0,0 +1,13 @@
+package com.iz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzApplication.class, args);
+	}
+
+}
---
+++ iz/src/test/java/com/iz/IzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
