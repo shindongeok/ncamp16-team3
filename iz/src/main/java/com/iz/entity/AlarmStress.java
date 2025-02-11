2f94765688517057d16cab86ec448195de588628
김태현 <kokiyo1030@naver.com>
2025-02-11 23:44:18 +0900

    [ADD] entity/controller

--- iz/src/main/java/com/iz/entity/AlarmStress.java
+++ iz/src/main/java/com/iz/entity/AlarmStress.java
@@ -1,4 +1,14 @@
 package com.iz.entity;
 
+import lombok.Data;
+
+@Data
 public class AlarmStress {
+    /**
+     * Stress랑 비슷한데
+     * 이걸 의도하신건지..?
+     */
+    private String member_id;
+    private int stress_num;
+
 }
