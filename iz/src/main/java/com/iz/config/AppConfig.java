0cc70d54b354aabea4bc76ce47894b107b5c08eb
hahyeong <hahyeong_0817@ewhain.net>
2025-02-11 20:31:57 +0900

    [DEL] Google font 적용 취소

---
+++ iz/src/main/java/com/iz/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.iz.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
--- iz/src/main/java/com/iz/controller/UserController.java
+++ iz/src/main/java/com/iz/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.iz.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
     @GetMapping("/update")
     public String update() {
         return "update";
