f55bebc2a205275eb9fc1ea63ca189439e826933
shindongeok <ehrflqakstp@gmail.com>
2025-02-11 21:22:26 +0900

    [ADD]게시판 HTML 화면 1차구현

---
+++ iz/src/main/resources/static/js/postDetail.js
@@ -0,0 +1,17 @@
+
+
+function openDeleteModal() {
+    document.getElementById("communityDeleteModal").classList.remove("hidden");
+}
+
+function closeDeleteModal() {
+    document.getElementById("communityDeleteModal").classList.add("hidden");
+}
+
+function openUpdateModal() {
+    document.getElementById("communityUpdateModal").classList.remove("hidden");
+}
+
+function closeUpdateModal() {
+    document.getElementById("communityUpdateModal").classList.add("hidden");
+}
