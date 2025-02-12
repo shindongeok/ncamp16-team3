8b3ccaea6fbd3bf0c8ef554eb0a1847cde71c9d1
김태현 <kokiyo1030@naver.com>
2025-02-12 15:40:49 +0900

    [ADD] 로그인 구현/회원가입 수정

--- iz/src/main/java/com/iz/controller/UserController.java
+++ iz/src/main/java/com/iz/controller/UserController.java
@@ -3,6 +3,7 @@ package com.iz.controller;
 import com.iz.entity.User;
 import com.iz.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -23,12 +25,11 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(User user, HttpSession session, Model model) {
+    public String register(User user) {
         timeSubstring(user);
 
         userService.register(user);
-        session.setAttribute("user", user);
-        return "main";
+        return "/";
     }
 
     @GetMapping("/update")
@@ -45,11 +46,41 @@ public class UserController {
         String startTime = user.getStart_time();
         String endTime = user.getEnd_time();
         String lunchTime = user.getLunch_time();
-        startTime = startTime.substring(0, 5) + ":00";
-        endTime = endTime.substring(0, 5) + ":00";
-        lunchTime = lunchTime.substring(0, 5) + ":00";
+
+        startTime = AmPm(startTime);
+        endTime = AmPm(endTime);
+        lunchTime = AmPm(lunchTime);
+
         user.setStart_time(startTime);
         user.setEnd_time(endTime);
         user.setLunch_time(lunchTime);
+    }
+
+    private static String AmPm(String time) {
+        String meridian = time.substring(6);
+//        log.debug("meridian: " + meridian);
+
+        if (meridian.equals("PM")) {
+            String result;
+            int hour = Integer.parseInt(time.substring(0, 2));
+            if (hour == 12) {
+                time = time.substring(0, 5) + ":00";
+                return time;
+            }
+//            log.debug("hour: " + hour);
+            String stringHour = String.valueOf(hour + 12);
+//            log.debug("stringHour: " + stringHour);
+
+            String substring = time.substring(2);
+            result = stringHour.concat(substring);
+//            log.debug("result: " + result);
+
+            time = result.substring(0, 5) + ":00";
+//            log.debug("startTime: " + time);
+        } else {
+            time = time.substring(0, 5) + ":00";
+//            log.debug("startTime: " + time);
+        }
+        return time;
     }
 }
