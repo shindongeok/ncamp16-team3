a6bf8554bef6c4a97d1fa4c90cee99fb0d8ad0d5
Seungmin <seungmin2055@gmail.com>
2025-02-13 11:09:46 +0900

    [FEAT]:채용공고 구현 초안 [ADD]: 찜한 공고 추가

--- iz/src/main/java/com/izikgram/job/repository/JobMapper.java
+++ iz/src/main/java/com/izikgram/job/repository/JobMapper.java
@@ -1,4 +1,17 @@
 package com.izikgram.job.repository;
 
+import com.izikgram.user.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface JobMapper {
+
+
+    @Select("select * from iz_member where member_id=#{member_id}")
+    User getUserById(@Param("member_id") String member_id);
+
+
+
 }
