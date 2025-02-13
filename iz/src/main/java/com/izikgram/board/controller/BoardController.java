e1433cca15e51cf232dc509a181fbcbd2f264f7e
shindongeok <ehrflqakstp@gmail.com>
2025-02-13 14:14:49 +0900

    [ADD]자유/하소연 게시글작성 기능 구현

--- iz/src/main/java/com/izikgram/board/controller/BoardController.java
+++ iz/src/main/java/com/izikgram/board/controller/BoardController.java
@@ -3,11 +3,10 @@ package com.izikgram.board.controller;
 import com.izikgram.board.entity.Board;
 import com.izikgram.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,22 +19,63 @@ public class BoardController {
 
     //자유,하소연 게시판
     @GetMapping("/{board_type}")
-    public String boardCommunity(@PathVariable("board_type") int boardType, Model model){
+    public String boardCommunity(@PathVariable("board_type") int board_type, Model model){
 
-        if (boardType != 1 && boardType != 2) {
+        if (board_type != 1 && board_type != 2) {
             model.addAttribute("error", "유효하지 않은 게시판 타입입니다.");
             return "redirect:/";
         }
 
-        String boardName = boardService.findBoardName(boardType);
-        List<Board> listBoard  = boardService.findBoardList(boardType);
+        String board_name = boardService.findBoardName(board_type);
+        List<Board> listBoard  = boardService.findBoardList(board_type);
         System.out.println("list :" + listBoard);
-        System.out.println(boardType);
-        model.addAttribute("boardName", boardName);
+        System.out.println(board_type);
+        model.addAttribute("board_name", board_name);
         model.addAttribute("listBoard", listBoard);
-        model.addAttribute("boardType", boardType);
+        model.addAttribute("board_type", board_type);
 
         return "/board/community";
+    }
+
+
+    //자유,하소연 작성하기 페이지 이동
+    @GetMapping("/postForm")
+    public String postForm(@RequestParam("board_type") int board_type, Model model){
+
+        if (board_type != 1 && board_type != 2) {
+            model.addAttribute("error", "유효하지 않은 게시판 타입입니다.");
+            return "redirect:/";
+        }
+        System.out.println(board_type);
+
+        String board_name = boardService.findBoardName(board_type);
+
+
+        model.addAttribute("board_name", board_name);
+        model.addAttribute("board_type", board_type);
+
+
+        return "/board/postForm";
+    }
+
+    // 글작성
+    @PostMapping("/{board_type}/post")
+    public String insertPost(
+            @PathVariable("board_type") int board_type,
+            @RequestParam("writer_id") String writer_id,
+            @RequestParam("title") String title,
+            @RequestParam("content") String content) {
+
+        // board_type 유효성 검사
+        if (board_type != 1 && board_type != 2) {
+            return "/index";
+        }
+
+        // 게시글 삽입
+        boardService.insertPost(board_type, writer_id, title, content);
+        System.out.println(board_type+ "," + writer_id + "," + title + "," + content);
+
+        return "redirect:/board/" + board_type;
     }
 
     //인기게시판
@@ -50,9 +90,4 @@ public class BoardController {
         return "/board/postDetail";
     }
 
-    //자유,하소연 수정
-    @GetMapping("update")
-    public String updatePost(){
-        return "/board/postForm";
-    }
 }
