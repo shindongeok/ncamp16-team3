65c05083bc77d06442eff148b9b40ab49b8882b5
shindongeok <ehrflqakstp@gmail.com>
2025-02-13 14:16:08 +0900

    [ADD]자유/하소연 게시글작성 기능 구현

--- iz/src/main/java/com/izikgram/board/controller/BoardController.java
+++ iz/src/main/java/com/izikgram/board/controller/BoardController.java
@@ -3,7 +3,6 @@ package com.izikgram.board.controller;
 import com.izikgram.board.entity.Board;
 import com.izikgram.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
