7ef29eda70a54ffb828c205a08acfa8481733f41
hahyeong <hahyeong_0817@ewhain.net>
2025-02-14 16:25:47 +0900

    [ADD] 메인페이지 인기게시글 기능 구현

--- iz/src/main/java/com/izikgram/main/controller/MainController.java
+++ iz/src/main/java/com/izikgram/main/controller/MainController.java
@@ -1,17 +1,40 @@
 package com.izikgram.main.controller;
 
+import com.izikgram.board.entity.Board;
+import com.izikgram.main.service.MainService;
+import com.izikgram.user.entity.User;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/main")
 @RequiredArgsConstructor
 public class MainController {
 
+    private final MainService mainService;
+
     @GetMapping
-    public String main() {
+    public String main(Model model, HttpSession session) {
+
+        // 인기글 3개 가져오기
+        List<Board> popularBoardList = mainService.getPopularBoardList();
+        model.addAttribute("popularBoardList", popularBoardList);
+
+        // 캘린더 feeling 가져오기
+        User user = (User) session.getAttribute("user");
+        LocalDate now = LocalDate.now();
+        List<Map<String, Object>> feelingList = mainService.getMonthlyFeeling(user.getMember_id(), now.format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM")));
+        model.addAttribute("feelingList", feelingList);
+
         return "/main/main";
     }
 }
