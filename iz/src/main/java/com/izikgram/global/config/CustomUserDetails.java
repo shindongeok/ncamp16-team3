f4a90e58889b1bc5fbc42e58e0227c83ca7c9626
kisuk <cks71714@naver.com>
2025-02-16 21:06:01 +0900

    [ADD] Spring Security 로그인실패 핸들러 추가 [ADD] 세션 관리 기능 추가

--- iz/src/main/java/com/izikgram/global/config/CustomUserDetails.java
+++ iz/src/main/java/com/izikgram/global/config/CustomUserDetails.java
@@ -4,11 +4,8 @@ import com.izikgram.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -16,53 +13,6 @@ import java.util.List;
 public class CustomUserDetails implements UserDetails {
 
      private User user;
-
-
-
-//    // UserDetails 필수 필드
-//    private final String username;
-//    private final String password;
-//    private final Collection<? extends GrantedAuthority> authorities;
-//
-//    // User 엔티티의 추가 필드들
-//    private final String name;
-//    private final String phone_num;
-//    private final String nickname;
-//    private final String birth_date;
-//    private final String email;
-//    private final int payday;
-//    private final String gender;
-//    private final String start_time;
-//    private final String lunch_time;
-//    private final String end_time;
-//    private final String intro;
-//    private final String loc_Mod;
-//    private final String ind_Cd;
-//    private final String edu_Lv;
-
-
-//    public CustomUserDetails(User user) {
-//        this.username = user.getMember_id();  // member_id를 username으로 사용
-//        this.password = "{noop}" + user.getPassword(); // 암호화 안해놔서 일단 임시 처리
-////        this.password = user.getPassword();
-//        this.name = user.getName();
-//        this.phone_num = user.getPhone_num();
-//        this.nickname = user.getNickname();
-//        this.birth_date = user.getBirth_date();
-//        this.email = user.getEmail();
-//        this.payday = user.getPayday();
-//        this.gender = user.getGender();
-//        this.start_time = user.getStart_time();
-//        this.lunch_time = user.getLunch_time();
-//        this.end_time = user.getEnd_time();
-//        this.intro = user.getIntro();
-//        this.loc_Mod = user.getLoc_mod();
-//        this.ind_Cd = user.getInd_cd();
-//        this.edu_Lv = user.getEdu_lv();
-//
-//        // 기본 권한 설정 (필요에 따라 수정)
-//        this.authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-//    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- iz/src/main/java/com/izikgram/user/controller/LoginController.java
+++ iz/src/main/java/com/izikgram/user/controller/LoginController.java
@@ -20,24 +20,24 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @GetMapping("/")
-    public String loginPage() {
-        return "index"; // 로그인 페이지 반환
-    }
+//    @GetMapping("/")
+//    public String loginPage() {
+//        return "index"; // 로그인 페이지 반환
+//    }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(String member_id, String password, HttpSession session) {
-//        log.info("member_id: " + member_id);
-//        log.info("password: " + password);
-        User user = loginService.login(member_id, password);
-        if (user != null) {
-            log.info("로그인객체: {}", user);
-            session.setAttribute("user", user);
-            return ResponseEntity.ok().body(Map.of("status", "success"));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("status", "error", "message", "아이디 또는 비밀번호를 확인해주세요"));
-        }
-    }
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(String member_id, String password, HttpSession session) {
+////        log.info("member_id: " + member_id);
+////        log.info("password: " + password);
+//        User user = loginService.login(member_id, password);
+//        if (user != null) {
+//            log.info("로그인객체: {}", user);
+//            session.setAttribute("user", user);
+//            return ResponseEntity.ok().body(Map.of("status", "success"));
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("status", "error", "message", "아이디 또는 비밀번호를 확인해주세요"));
+//        }
+//    }
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
