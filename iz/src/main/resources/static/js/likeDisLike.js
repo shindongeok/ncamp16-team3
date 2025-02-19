6f2a550b679364ec2d30b6b387d1b3ff972f8a78
shindongeok <ehrflqakstp@gmail.com>
2025-02-19 19:11:08 +0900

    [FIX] 게시판페이지 board_type 넘기는데 제대로안되는거?

---
+++ iz/src/main/resources/static/js/likeDisLike.js
@@ -0,0 +1,44 @@
+// 이직해 존버해 버튼
+const boardType = document.getElementById("boardType").value;
+console.log("dfdf: "+ boardType)
+
+console.log(boardId);
+console.log(boardType);
+
+
+// 좋아요/싫어요 토글 처리
+function toggleLikeDislike(boardId, type) {
+    console.log("좋아요/싫어요 요청");
+
+    const isLiked = type === "like";
+
+    $.ajax({
+        url: '/board/' + boardId + '/' + boardType + '/update', // URL 요청
+        method: 'POST',
+        data: {
+            isLiked: isLiked  // TODO true or false
+        },
+        success: function(response) {
+            console.log("좋아요, 싫어요 개수 가져오기 성공!");
+
+            // 화면 배치
+            let likeEl = document.getElementById('likeCount');
+            let dislikeEl = document.getElementById('dislikeCount');
+
+            console.log(response.like);
+            console.log(response.dislike);
+
+            console.log(likeEl);
+            console.log(dislikeEl);
+
+            $("#likeCount").text(response.like);
+            $("#dislikeCount").text(response.dislike);
+
+            // likeEl.innerText(response.like);
+            // dislikeEl.innerText(response.dislike);
+        },
+        error: function(xhr, status, error) {
+            console.error("Error:", error);
+        }
+    });
+}
