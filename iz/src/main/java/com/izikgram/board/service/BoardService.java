6a95dae477b96814ffd6fc5a2dbe1a2579d27df4
shindongeok <ehrflqakstp@gmail.com>
2025-02-24 20:36:24 +0900

    [CHORE] css 전체 수정 [ADD] feelMyBoard.html 내 자유게시글 생성 [ADD] whiningMyBoard.html 내 하소연 게시글 생성

--- iz/src/main/java/com/izikgram/board/service/BoardService.java
+++ iz/src/main/java/com/izikgram/board/service/BoardService.java
@@ -4,10 +4,13 @@ import com.izikgram.alarm.entity.AlarmType;
 import com.izikgram.alarm.service.AlarmService;
 import com.izikgram.alarm.service.SseEmitterService;
 import com.izikgram.board.entity.Board;
+
 import com.izikgram.board.entity.BoardDto;
+import com.izikgram.board.entity.Comment;
 import com.izikgram.board.entity.CommentDto;
 import com.izikgram.board.repository.BoardMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
