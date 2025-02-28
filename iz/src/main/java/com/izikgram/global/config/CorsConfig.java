4af0d5edf70a33fa6ca8b6f8b7ef0a5cc297a7bc
kisuk <cks71714@naver.com>
2025-02-28 11:45:10 +0900

    [FIX] cors 문제 해결

---
+++ iz/src/main/java/com/izikgram/global/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.izikgram.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://223.130.151.184:8080")); // 허용할 Origin
+        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 인증 정보 포함 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 CORS 설정 적용
+
+        return source;
+    }
+}
