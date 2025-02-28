d1923ca74d0f41a9883878ef6bea66bde3397bbc
kisuk <cks71714@naver.com>
2025-02-28 11:51:29 +0900

    [FIX] 롤백

--- iz/src/main/java/com/izikgram/global/security/SecurityConfig.java
+++ iz/src/main/java/com/izikgram/global/security/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
 
     private final CustomLoginSuccessHandler loginSuccessHandler;
 
-    private CorsConfig corsConfig;
+//    private CorsConfig corsConfig;
 
     public SecurityConfig(CustomLoginSuccessHandler loginSuccessHandler) {
         this.loginSuccessHandler = loginSuccessHandler;
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource())) // CORS 설정
+//                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource())) // CORS 설정
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화 (SSE 때문에 비활성화 해야함)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/user/findId", "/user/findIdResult", "/user/findPw", "/user/findPwResult", "/user/register", "/user/checkId/**", "/user/resetPw").permitAll() // 로그인 없이 접근 가능
